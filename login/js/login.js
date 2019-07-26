be00f66a7884ae0d6d80e196d35df20281d003b3
wykolor <kolor1121460147@163.com>
2019-07-26 18:04:06 +0800

    。。。。

--- login/js/login.js
+++ login/js/login.js
@@ -1,23 +1,23 @@
 // 保证页面DOM加载完成并且能够成功获取
-;$(function($){
+; $(function ($) {
     // 点击验证码登录
-    $(".isyancode").click(function(){
+    $(".isyancode").click(function () {
         $(this).hide();
         $(".pwd").hide();
         $(".ispwd").show();
         $(".yanzheng").show();
     });
     // 点击密码登录效果
-    $(".ispwd").click(function(){
+    $(".ispwd").click(function () {
         $(this).hide();
         $(".yanzheng").hide();
         $(".isyancode").show();
         $(".pwd").show();
     });
     // 表单提交事件,获取表单序列化数据
-    $("#sumbit").click(function(event){
+    $("#sumbit").click(function (event) {
         event.preventDefault();
-        console.log('表单数据',$("#form").serializeArray())
+        console.log('表单数据', $("#form").serializeArray())
         $(this).hide();
         $(".logining").show();
     })
@@ -49,10 +49,11 @@
         }
     };
     // 
-    $("#getcode").click(function(){
-        $('#yancode').css({width:'57%'})
+    $("#getcode").click(function () {
+        $('#yancode').css({ width: '57%' })
         $("#timeCut").show();
         $(this).hide();
         countDown(60);
-    })
+    });
+
 })
